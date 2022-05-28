67157ebbb0bdbd3010685dee5176fb0cb0e1cd1c
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-05-28 18:46:37 +0400

    switching to class-based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,16 @@
-import classes from './User.module.css';
+import { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from "./User.module.css";
+
+class User extends Component {
+  // we can add as many methods as we want: render(), constructor() ...
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
